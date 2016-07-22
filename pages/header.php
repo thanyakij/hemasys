02c68e0143977ddddfc4fe362092ee82ad84f6fd
Wuttikrai Limsakul <hiphopskynew@gmail.com>
2016-07-22 09:41:41 +0700

    include angular directive

--- pages/header.php
+++ pages/header.php
@@ -9,6 +9,16 @@
       <script src="assets/js/ng-module/angular-module.js"></script>
       <script src="assets/js/ng-module/moment.min.js"></script>
       <script src="assets/js/ng-module/variables-service.js"></script>
+      <script src="assets/js/ng-directive/date-input.js"></script>
+      <script src="assets/js/ng-directive/is-disabled.js"></script>
+      <script src="assets/js/ng-directive/is-required.js"></script>
+      <script src="assets/js/ng-directive/lowercase.js"></script>
+      <script src="assets/js/ng-directive/max-length.js"></script>
+      <script src="assets/js/ng-directive/number-only.js"></script>
+      <script src="assets/js/ng-directive/string-and-number.js"></script>
+      <script src="assets/js/ng-directive/string-only.js"></script>
+      <script src="assets/js/ng-directive/uppercase.js"></script>
+      <script src="assets/js/ng-directive/valid-email.js"></script>
       <script src="assets/js/jquery-2.2.1.js"></script>
       <script src="assets/js/bootstrap.min.js"></script>
       <style type="text/css">
