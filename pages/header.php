08c2230268b9554c4e720ec71d8fd4528d6f51dc
Aqua <Vanijvarmindra.aqua@gmail.com>
2016-07-27 06:47:07 +0700

    re-structure + MetroUI

--- pages/header.php
+++ pages/header.php
@@ -4,9 +4,13 @@
       <link rel="stylesheet" type="text/css" href="assets/jquery-ui/jquery-ui.min.css">
       <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
       <link rel="stylesheet" type="text/css" href="assets/css/materialize.min.css" />
+      <link rel="stylesheet" type="text/css" href="assets/css/metro-ui/metro.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/metro-ui/metro-icons.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/metro-ui/metro-colors.css">
       <link rel="stylesheet" type="text/css" href="assets/css/main.css">
       <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-      <link rel="stylesheet" type="text/css" href="assets/css/_UI12col.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/_metronacci.css">
+      <link rel="stylesheet" type="text/css" href="assets/css/_colour.css">
       
       <script src="assets/js/components/scripts.js"></script>
       <script src="assets/js/jquery-2.2.1.js"></script>
@@ -14,6 +18,7 @@
       
       <script src="assets/js/jquery.min.js"></script>
       <script src="assets/js/materialize.min.js"></script>	
+      <script src="assets/js/metro-ui/metro.js"></script>
       <?php
 
       include ("pages/setting.php");
