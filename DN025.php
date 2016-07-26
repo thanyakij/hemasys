387ac6a59387c83b11eefeb13fa7943d5223f060
promphut <phiphut.ch@gmail.com>
2016-07-26 09:28:27 +0700

    aaa

--- DN025.php
+++ DN025.php
@@ -15,6 +15,42 @@ include ("pages/header.php");
 <link rel="stylesheet" href="assets/css/main.css" type="text/css"/>
 
 <script type="text/javascript">
+    $(window).load(function(){
+        $.urlParam = function (name){
+            var param = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+            if(param == null){return null}else{return param[1]||0}
+        }
+        var donorId = $.urlParam("donor_id")
+        var url = "http://192.168.0.145/api/donor/read/"+donorId
+        $.get( url, function( data ) {
+            console.log(data.SEX_OF_DONOR)
+            if(data.SEX_OF_DONOR=="1"){
+                $("#mr1").attr({"checked":true})
+            }
+            else if(data.SEX_OF_DONOR=="2"){
+                $("#mr2").attr({"checked":true})
+            }
+            else if(data.SEX_OF_DONOR=="3"){
+                $("#mr3").attr({"checked":true})
+            }
+            else if(data.SEX_OF_DONOR=="4"){
+                $("#mr4").attr({"checked":true})
+            }
+            $("input[name=Fname]").val(data.FIRST_NAME)
+            $("input[name=Sname]").val(data.SURNAME)
+            $("input[name=dob]").val(data.FIRST_NAME)
+            $("#addr").val(data.ADDRESS)
+            $(".id").html(data.DONOR_ID)
+            $("input[name=postCode]").val(data.POSTAL_OR_ZIP_CODE)
+            $("input[name=town]").val(data.MARRIED_TOWN)
+            $("input[name=rout]").val(data.ROUT_CODE)
+            $("input[name=country]").val(data.CODE_OF_COUNTRY_OF_NATIONALITY)
+            $("input[name=county]").val()
+            $("select[name=region]").val(data.REGION_CODE)
+            $("input[name=rout]").val(data.ROUT_CODE)
+        });
+    })
+
 	$(function(){		
 		$("#dob").val("18-ธ.ค-2528");
 		$("#calDob").html(calDOB(strToDate($("#dob").val()))+" years");
@@ -25,13 +61,14 @@ include ("pages/header.php");
 		$("#dateTimeOfSearch").val(getCurrentDateTimeString());
 		$(".date").html(getCurrentDateTimeString());
 	});
+    
 </script>
 <div id="<?php echo $pageClass; ?>" class="container">
     <!--Donor ID-->
     <div class="panel panel-default">
         <div class="panel-body text-center">
         <strong>Donor : </strong>
-        <span class="id">1000212312<span> - 
+        <span class="id"><span> - 
         <strong>Administrative coordinates</strong> - 
         <span class="date">8 Dec 2016</span>
         </div>
@@ -46,13 +83,13 @@ include ("pages/header.php");
                 </div>
                 <div class="panel-body">
                     <div class="form-group">
-                        <input type="radio" name="mr" tabindex="1">
+                        <input type="radio" name="mr" id="mr1" value="1" tabindex="1">
                         <label for="mr">Mr.</label>
-                        <input type="radio" name="mr" tabindex="2">
+                        <input type="radio" name="mr" id="mr2" value="2" tabindex="2">
                         <label for="mr">Mrs.</label>
-                        <input type="radio" name="mr" tabindex="3">
+                        <input type="radio" name="mr" id="mr3" value="3" tabindex="3">
                         <label for="mr">Miss.</label>
-                        <input type="radio" name="mr" tabindex="4">
+                        <input type="radio" name="mr" id="mr4" value="4" tabindex="4">
                         <label for="mr">???</label>
                     </div>
                     <div class="form-group">
@@ -111,7 +148,7 @@ include ("pages/header.php");
                                 <label for="">Country</label>
                             </div>
                             <div class="col-sm-9">
-                                <input type="text" name="country-select" class="form-control" tabindex="9">
+                                <input type="text" name="country" class="form-control" tabindex="9">
                             </div>
                         </div>
                     </div>
@@ -121,7 +158,7 @@ include ("pages/header.php");
                                 <label for="">Region</label>
                             </div>
                             <div class="col-sm-7">
-                                <select class="form-control" tabindex="10">
+                                <select name="region" class="form-control" tabindex="10">
                                     <option>Oprion 1</option>
                                     <option>Oprion 2</option>
                                     <option>Oprion 3</option>
@@ -137,10 +174,10 @@ include ("pages/header.php");
                     <div class="form-group">
                         <div class="row">
                             <div class="col-sm-3">
-                                <label for="">Country</label>
+                                <label for="">County</label>
                             </div>
                             <div class="col-sm-9">
-                                <input type="text" name="country-txt" class="form-control" tabindex="12">
+                                <input type="text" name="county" class="form-control" tabindex="12">
                             </div>
                         </div>
                     </div>
@@ -161,7 +198,7 @@ include ("pages/header.php");
                                 <label for="">Address</label>
                             </div>
                             <div class="col-sm-10">
-                                <textarea class="form-control" tabindex="13">
+                                <textarea id="addr" class="form-control" tabindex="13">
                                 </textarea>
                             </div>
                         </div>
@@ -172,7 +209,7 @@ include ("pages/header.php");
                                 <label for="">Town</label>
                             </div>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" tabindex="14">
+                                <input type="text" name="town" class="form-control" tabindex="14">
                             </div>
                         </div>
                     </div>
@@ -182,7 +219,7 @@ include ("pages/header.php");
                                 <label for="">Post Code/Country</label>
                             </div>
                             <div class="col-sm-9">
-                                <input type="text" class="form-control" tabindex="15">
+                                <input type="text" name="postCode" class="form-control" tabindex="15">
                             </div>
                         </div>
                     </div>
@@ -192,7 +229,7 @@ include ("pages/header.php");
                                 <label for="">Rout</label>
                             </div>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" tabindex="16">
+                                <input type="text" name="rout" class="form-control" tabindex="16">
                             </div>
                         </div>
                     </div>
@@ -223,7 +260,7 @@ include ("pages/header.php");
                                 <label for="">Region</label>
                             </div>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" tabindex="19">
+                                <input type="text" name="region1" class="form-control" tabindex="19">
                             </div>
                         </div>
                     </div>
@@ -233,7 +270,7 @@ include ("pages/header.php");
                                 <label for="">Region</label>
                             </div>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" tabindex="20">
+                                <input type="text" name="region2" class="form-control" tabindex="20">
                             </div>
                         </div>
                     </div>
@@ -268,7 +305,7 @@ include ("pages/header.php");
                     </div>
                 </div>
                 <div class="col-sm-6 text-right">
-                    <button class="btn btn-info" tabindex="23"> Search</button>
+                    <button type="button" onclick="search()" class="btn btn-info" tabindex="23"> Search</button>
                     <button class="btn btn-danger" tabindex="24"> Exit</button>
                 </div>
             </div>
