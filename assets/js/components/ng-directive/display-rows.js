109f57e041ee462c559184c7bdac22c4fe91f794
Wuttikrai Limsakul <hiphopskynew@gmail.com>
2016-07-28 18:06:49 +0700

    bootstrap

--- assets/js/components/ng-directive/display-rows.js
+++ assets/js/components/ng-directive/display-rows.js
@@ -1,63 +1,7 @@
-app.directive('displayRows', function() {
-  var block_width = '100%';
-
-  function addStylesheet() {
-    angular
-      .element(document)
-      .find('head')
-      .prepend('<style type="text/css">tr:after { content: \'\'; display: block; visibility: hidden; clear: both; }</style>');
-  }
-
-  function setStylesheet(elem, height) {
-    elem.style.height = height+"px";
-    elem.style.overflow = "auto";
-  }
-
-  function setWidthOfBlock(elem, width) {
-    elem.style.width = width;
-    elem.style.float = 'left';
-  }
-
-  function setDisplay(elems) {
-    for (var i=0; i<elems.length; i++) {
-      var elem = elems[i];
-      elem.style.display = "block";
-      if (elem.tagName == "TH" || elem.tagName == "TD") setWidthOfBlock(elem, block_width);
-    }
-  }
-
+app.directive('displayRows', ['DTDefaultOptions', function(DTDefaultOptions) {
   return {
-    require: '?ngModel',
-    link: function(scope, element, attrs, ctrl) {
-      if (!ctrl) return;
-      var number = attrs.displayRows, trs = element.find('tr'), limit_height = 0, temp = 0, elements = [];
-      block_width = (100 / element.find('th').length) + '%';
-      if (!number) (number = 10);
-      number = parseInt(number);
-
-      for (var i=0; i < number; i++) {
-        try {
-          if (trs[i].children[0].tagName == 'TD') {
-            temp = trs[i].offsetHeight;
-          } else {
-            number++;
-          }
-        } catch (err) {}
-
-        limit_height += temp;
-      }
-
-      addStylesheet();
-      setDisplay(element.find('th'));
-      setDisplay(element.find('tbody'));
-      setDisplay(element.find('thead'));
-      setDisplay(element.find('tr'));
-      setDisplay(element.find('td'));
-
-      var children = element[0].children;
-      for (var i=0; i < children.length; i++) {
-        if (children[i].tagName == 'TBODY') setStylesheet(children[i], limit_height);
-      }
+    link: function(scope, element, attrs) {
+      if (parseInt(attrs.displayRows) > 0) DTDefaultOptions.setDisplayLength(attrs.displayRows);
     }
   };
-});
+}]);
--- assets/js/components/ng-module/angular-module.js
+++ assets/js/components/ng-module/angular-module.js
@@ -1,5 +1,30 @@
-const app = angular.module('hemasys', ['datatables']);
+const app = angular.module('hemasys', ['datatables', 'datatables.bootstrap']);
 
-app.run(function(DTDefaultOptions) {
-  DTDefaultOptions.setDisplayLength(5);
-});
+/** DATA TABLE OPTIONS **/
+/*
+  DTOptionsBuilder
+    .newOptions()
+    .withOption('serverSide', true)
+    .withOption('hasBootstrap', true)
+    .withOption('processing', true)
+    .withDisplayLength(10)
+    .withPaginationType('full_numbers')
+    .withOption('order', [[ 0, 'desc' ]])
+    .withOption('stateSave', true)
+    .withOption('searchDelay', 350)
+    .withOption('width', '100%')
+    .withDataProp('data');
+*/
+
+app.run(['$rootScope', 'DTDefaultOptions', 'DTOptionsBuilder', function($rootScope, DTDefaultOptions, DTOptionsBuilder) {
+  DTDefaultOptions.setDisplayLength(5).setDOM('ftipr');
+
+  $rootScope.bootstrap = DTOptionsBuilder
+    .newOptions()
+    .withBootstrap()
+    // .setDOM('ftipr')
+    .withOption('hasBootstrap', true)
+    .withOption('processing', true)
+    .withOption('width', '100%')
+    .withDisplayLength(5);
+}]);
